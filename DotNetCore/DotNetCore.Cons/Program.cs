b0047f1067569fb5053a2864010baa0916ff9ac3
yzz <1277745281@qq.com>
2018-02-01 15:37:39 +0800

    -

--- DotNetCore/DotNetCore.Cons/Program.cs
+++ DotNetCore/DotNetCore.Cons/Program.cs
@@ -2,11 +2,13 @@
 
 namespace DotNetCore.Cons
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//Console.WriteLine("Hello World!");
+			Console.WriteLine($"Hello{ System.Environment.GetEnvironmentVariable("USER")}!I'm {System.Environment.MachineName} and I'm talkingto you from { System.IO.Directory.GetCurrentDirectory()}");
+			Console.ReadKey();
+		}
+	}
 }
