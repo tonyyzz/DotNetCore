08b9a3958360020ca6193fa319d2b747fc5b0bdd
yzz <1277745281@qq.com>
2018-02-02 18:01:31 +0800

    -

--- DotNetCore/DotNetCore.Cons/Program.cs
+++ DotNetCore/DotNetCore.Cons/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using Dapper;
+using Newtonsoft;
 
 namespace DotNetCore.Cons
 {
@@ -8,6 +10,7 @@ namespace DotNetCore.Cons
 		{
 			//Console.WriteLine("Hello World!");
 			Console.WriteLine($"Hello{ System.Environment.GetEnvironmentVariable("USER")}!I'm {System.Environment.MachineName} and I'm talkingto you from { System.IO.Directory.GetCurrentDirectory()}");
+			Console.WriteLine(Newtonsoft.Json.JsonConvert.SerializeObject(new { f = "4545dfh" }));
 			Console.ReadKey();
 		}
 	}
---
+++ DotNetCore/cli-samples/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace cli_samples.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
